d0427fa61c76e44762c9a6af02ee1e9df4840101
dilnawaz khan <dilnawaz@protogoup.co>
2024-11-22 21:18:42 +0500

    assets fonts add and linking of fonts

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        alias: {
+          '@src': './src',
+          '@commons': './src/commons/*',
+          '@components': './src/components',
+          '@configs': './src/configs/index.ts',
+          '@contexts': './src/contexts/index.ts',
+          '@screens': './src/screens/index.ts',
+          '@utils': './src/utils/index.ts',
+          '@assets': './src/assets/index.ts',
+          '@constants': './src/constants/index.ts',
+          '@routes': './src/routes/index.ts',
+          '@layouts': './src/layouts/index.ts',
+          '@hooks': './src/hooks/index.ts',
+          '@api': './src/api/index.ts',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/constants/fonts.ts
@@ -0,0 +1,14 @@
+const FONTS = {
+  POPPINS_XLIGHT: 'Poppins-ExtraLight',
+  POPPINS_LIGHT: 'Poppins-Light',
+  POPPINS_THIN: 'Poppins-Thin',
+  POPPINS_ITALIC: 'Poppins-Italic',
+  POPPINS_REGULAR: 'Poppins-Regular',
+  POPPINS_MEDIUM: 'Poppins-Medium',
+  POPPINS_SEMIBOLD: 'Poppins-SemiBold',
+  POPPINS_BOLD: 'Poppins-Bold',
+  POPPINS_XBOLD: 'Poppins-ExtraBold',
+  POPPINS_BLACK: 'Poppins-Black',
+};
+
+export default FONTS;
